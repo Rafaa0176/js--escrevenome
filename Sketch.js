4c224e8c552a290646a6a90f9f270adeede60aee
Rafaa0176 <rafinhaoliveiraribeiro22@gmail.com>
2024-08-07 12:13:40 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600, 600);
+ background("white");
+}
+
+function draw() {
+
+  stroke ("red")
+ fill("blue")
+
+ //console.log(mouseIsPressed)
+  
+ if(mouseIsPressed){
+ rect(mouseX,mouseY,20,30)
+}
+
+
+}
